5f45c342f3e0fd5fb9d017829ef9b9c93a8d1556
Ada <byjean94@gmail.com>
2022-08-16 13:57:01 +0200

    Add Skills Section

---
+++ src/Components/FirstSection/Skills/index.js
@@ -0,0 +1,19 @@
+import { Box, UnorderedList, ListItem, Heading } from '@chakra-ui/react';
+
+export function Skils() {
+    return <Box>
+        <Heading fontFamily="Montserrat" color="#FFE7D4" textTransform="uppercase">Umiejętności</Heading>
+        <UnorderedList>
+            <ListItem fontFamily="Montserrat" color="white">język angielski - poziom B2</ListItem>
+            <ListItem fontFamily="Montserrat" color="white">JavaScript - poziom podstawowy</ListItem>
+            <ListItem fontFamily="Montserrat" color="white">HTML i CSS - poziom podstawowy</ListItem>
+            <ListItem fontFamily="Montserrat" color="white">React - poziom podstawowy</ListItem>
+            <ListItem fontFamily="Montserrat" color="white">Git - poziom podstawowy</ListItem>
+            <ListItem fontFamily="Montserrat" color="white">prawo jazdy kat. B</ListItem>
+        </UnorderedList>
+    </Box>
+}
+
+
+
+
