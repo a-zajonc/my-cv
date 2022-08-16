29d8a315264776f9a5ec874ec5e64e243e7dfd9a
Ada <byjean94@gmail.com>
2022-08-16 13:33:14 +0200

    Add About Section

---
+++ src/Components/FirstSection/About/index.js
@@ -0,0 +1,10 @@
+import { Heading, Text, Box } from '@chakra-ui/react';
+
+export function About() {
+    return <Box>
+        <Heading fontFamily="Montserrat" color="#FFE7D4" textTransform="uppercase">O mnie</Heading>
+        <Text fontFamily="Montserrat" color="white" fontSize="15px">
+            Jestem osobą, która nie boi się wyzwań. Podjęcie się awansu w pracy z jednoczesnym studiowaniem czy przeprowadzka do innego miasta nie jest dla mnie większym problemem.             Podczas przebywania na urlopie macierzyńskim postanowiłam podjąć się kolejnego wyzwania, jakim jest nauka programowania. Zdecydowałam się na samodzielną naukę JavaScript, HTML oraz             CSS. Następnie wdrożyłam React, a niedawno zaczęłam korzystać z GitHuba oraz uczyć się Gita. Następnie w planach mam podjęcie się nauki TypeScripta.
+        </Text>
+    </Box>
+}
--- src/Components/FirstSection/index.js
+++ src/Components/FirstSection/index.js
@@ -2,11 +2,13 @@ import { Box, Text } from '@chakra-ui/react';
 import { PortfolioImage } from './Image';
 import "@fontsource/montserrat"
 import { NameHeader } from './NameHeader';
+import { About } from './About';
 
 export function FirstSection() {
     return <Box p={20} bg="#494E5F" width="700px">
         <PortfolioImage />
         <NameHeader />
+        <About />
         <Text fontFamily="Montserrat" color="white">About part</Text>
     </Box>
 }
