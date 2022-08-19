bf4461bcd0c94b1bf24098795b2e059cb31bef86
Ada <byjean94@gmail.com>
2022-08-19 11:35:45 +0200

    Removed duplicated lines in Contact Section

--- src/Components/FirstSection/Contact/index.js
+++ src/Components/FirstSection/Contact/index.js
@@ -5,35 +5,23 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 export function Contact() {
+
+    function contactDisplay(icon, information) {
+        return <ListItem display="flex" alignItems="center">
+            <FontAwesomeIcon icon={icon} color="white" width="16px" height="16px" />
+            <Text fontFamily="Montserrat" color="white" pl="10">
+                {information}
+            </Text>
+        </ListItem>
+    }
+
     return <Box>
         <Heading fontFamily="Montserrat" color="#FFE7D4" textTransform="uppercase" pb="5">Dane kontaktowe</Heading>
         <List pb="10" >
-            <ListItem display="flex" alignItems="center">
-                <FontAwesomeIcon icon={faPhone} color="white" />
-                <Text fontFamily="Montserrat" color="white" pl="10">
-                    668 954 072
-                </Text>
-            </ListItem>
-            <ListItem display="flex" alignItems="center">
-                <FontAwesomeIcon icon={faGithub} color="white" />
-                <Text fontFamily="Montserrat" color="white" pl="10">
-                    https://github.com/a-zajonc
-                </Text>
-            </ListItem>
-            <ListItem display="flex" alignItems="center">
-                <FontAwesomeIcon icon={faEnvelope} color="white" />
-                <Text fontFamily="Montserrat" color="white" pl="10">
-                    slaboszadriana@gmail.com
-                </Text>
-            </ListItem>
-            <ListItem display="flex" alignItems="center">
-                <FontAwesomeIcon icon={faLocationPin} color="white" />
-                <Text fontFamily="Montserrat" color="white" pl="10">
-                    Szczecin
-                </Text>
-            </ListItem>
+            {contactDisplay((faPhone), ("668 954 072"))}
+            {contactDisplay((faGithub), ("https://github.com/a-zajonc"))}
+            {contactDisplay((faEnvelope), ("slaboszadriana@gmail.com"))}
+            {contactDisplay((faLocationPin), ("Szczecin"))}
         </List>
     </Box>
 }
-
-/* <FontAwesomeIcon icon={faGithub} color="white" /> */
