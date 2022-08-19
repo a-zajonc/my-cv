3d8a01de8bf4c588f7aa95225cfb03cd5869e549
Ada <byjean94@gmail.com>
2022-08-19 14:45:49 +0200

    Improve Display of Employment Experience

--- src/Components/SecondSection/Employment/index.js
+++ src/Components/SecondSection/Employment/index.js
@@ -1,108 +1,39 @@
 import { Heading, Box, UnorderedList, ListItem, Accordion, AccordionButton, AccordionItem, AccordionIcon, AccordionPanel } from '@chakra-ui/react';
 import { Header } from '../Styling';
+import { work } from '../../../database';
 
 export function Employment() {
 
+    function workplaceDisplay(jobTitle, jobPlace, jobExperience) {
+        return <AccordionItem border="none" pt={3}>
+            <AccordionButton pl={0}>
+                <Heading fontSize="24px" fontFamily="Montserrat" color="#494E5F" flex='1' textAlign='left'>
+                    {jobTitle}
+                </Heading>
+                <AccordionIcon />
+            </AccordionButton>
+            <Heading fontSize="14px" fontFamily="Montserrat" color="#494E5F" fontWeight="300">
+                {jobPlace}
+            </Heading>
+            <AccordionPanel>
+                <UnorderedList color="#494E5F">
+                    {jobExperience.map((experience) => {
+                        return <ListItem fontFamily="Montserrat">{experience}</ListItem>
+                    }
+                    )}
+                </UnorderedList>
+            </AccordionPanel>
+        </AccordionItem>
+
+    }
+
     return <Box p="5">
         <Header title={"Historia zatrudnienia"} />
-        <Accordion allowMultiple >
-            <AccordionItem border="none" pt={3}>
-                <AccordionButton pl={0}>
-                    <Heading fontSize="24px" fontFamily="Montserrat" color="#494E5F" flex='1' textAlign='left'>
-                        Kierownik Działu w H&M
-                    </Heading>
-                    <AccordionIcon />
-                </AccordionButton>
-                <Heading fontSize="14px" fontFamily="Montserrat" color="#494E5F" fontWeight="300">
-                    Centrum Handlowe Marino, Wrocław (06.2018 - 08.2020, w tym Urlop Macierzyński: 08.2019-08.2020)
-                </Heading>
-                <AccordionPanel>
-                    <UnorderedList color="#494E5F">
-                        <ListItem fontFamily="Montserrat">Rekrutacja, szkolenia, ocena i rozwój doradców sprzedaży w zespole</ListItem>
-                        <ListItem fontFamily="Montserrat">Zarządzanie zespołem wraz z planowaniem czasu pracy sobie i innym członkom zespołu</ListItem>
-                        <ListItem fontFamily="Montserrat">Nadzorowanie pracy oraz przekazywanie feedbacków</ListItem>
-                        <ListItem fontFamily="Montserrat">Inicjowanie oraz kontrolowanie działań mających na celu zapewnienie zysku i zapobieganie stratom</ListItem>
-                        <ListItem fontFamily="Montserrat">Umiejętność planowania i reagowania na dynamicznie rozwijające się sytuacje poprzez ustalanie odpowiednich priorytetów</ListItem>
-                        <ListItem fontFamily="Montserrat"> Analiza i reakcja na raporty sprzedaży</ListItem>
-                    </UnorderedList>
-                </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem border="none" pt={3}>
-                <AccordionButton pl={0} >
-                    <Heading fontSize="24px" fontFamily="Montserrat" color="#494E5F" flex='1' textAlign='left'>
-                        Lider Zmiany w H&M
-                    </Heading>
-                    <AccordionIcon />
-                </AccordionButton>
-                <Heading fontSize="14px" fontFamily="Montserrat" color="#494E5F" fontWeight="300" >
-                    Szczecin (02.2018 - 05.2018)
-                </Heading>
-                <AccordionPanel>
-                    <UnorderedList color="#494E5F">
-                        <ListItem fontFamily="Montserrat">Wdrażanie nowych pracowników</ListItem>
-                        <ListItem fontFamily="Montserrat">Zarządzanie zespołem</ListItem>
-                        <ListItem fontFamily="Montserrat">Bezpośrednia i pośrednia obsługa klienta</ListItem>
-                        <ListItem fontFamily="Montserrat">Wykonywanie zadań związanych z operacjami gotówkowymi oraz księgowością</ListItem>
-                    </UnorderedList>
-                </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem border="none" pt={3}>
-                <AccordionButton pl={0}>
-                    <Heading fontSize="24px" fontFamily="Montserrat" color="#494E5F" flex='1' textAlign='left'>
-                        Doradca Sprzedaży w H&M
-                    </Heading>
-                    <AccordionIcon />
-                </AccordionButton>
-                <Heading fontSize="14px" fontFamily="Montserrat" color="#494E5F" fontWeight="300">
-                    Centrum Handlowe Galaxy, Szczecin (07.2015 - 05.2018)
-                </Heading>
-                <AccordionPanel>
-                    <UnorderedList color="#494E5F">
-                        <ListItem fontFamily="Montserrat">Obsługa klienta</ListItem>
-                        <ListItem fontFamily="Montserrat">Obsługa kasy</ListItem>
-                        <ListItem fontFamily="Montserrat">Praca z towarem</ListItem>
-                        <ListItem fontFamily="Montserrat">Praca zespołowa</ListItem>
-                    </UnorderedList>
-                </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem border="none" pt={3}>
-                <AccordionButton pl={0}>
-                    <Heading fontSize="24px" fontFamily="Montserrat" color="#494E5F" flex='1' textAlign='left'>
-                        Kasjer-Sprzedawca w Reserved
-                    </Heading>
-                    <AccordionIcon />
-                </AccordionButton>
-                <Heading fontSize="14px" fontFamily="Montserrat" color="#494E5F" fontWeight="300">
-                    Centrum Handlowe Kaskada, Szczecin (03.2014 - 06.2015)
-                </Heading>
-                <AccordionPanel>
-                    <UnorderedList color="#494E5F">
-                        <ListItem fontFamily="Montserrat">Coś</ListItem>
-                        <ListItem fontFamily="Montserrat">Po</ListItem>
-                        <ListItem fontFamily="Montserrat">zmie</ListItem>
-                        <ListItem fontFamily="Montserrat">niać!</ListItem>
-                    </UnorderedList>
-                </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem border="none" pt={3}>
-                <AccordionButton pl={0}>
-                    <Heading fontSize="24px" fontFamily="Montserrat" color="#494E5F" flex='1' textAlign='left'>
-                        Barmanka
-                    </Heading>
-                    <AccordionIcon />
-                </AccordionButton>
-                <Heading fontSize="14px" fontFamily="Montserrat" color="#494E5F" fontWeight="300">
-                    Restauracja Bombay, Szczecin (10.2013 - 02.2014)
-                </Heading>
-                <AccordionPanel>
-                    <UnorderedList color="#494E5F">
-                        <ListItem fontFamily="Montserrat">Coś</ListItem>
-                        <ListItem fontFamily="Montserrat">Po</ListItem>
-                        <ListItem fontFamily="Montserrat">zmie</ListItem>
-                        <ListItem fontFamily="Montserrat">niać!</ListItem>
-                    </UnorderedList>
-                </AccordionPanel>
-            </AccordionItem>
+        <Accordion allowMultiple>
+            {work.map((job) => {
+                return workplaceDisplay((job.jobTitle), (job.place), (job.jobDescription))
+            }
+            )}
         </Accordion>
     </Box>
 }
---
+++ src/database.js
@@ -0,0 +1,63 @@
+export const work = [
+    {
+        jobTitle: "Kierownik Działu w H&M",
+        place: "Centrum Handlowe Marino, Wrocław (06.2018 - 08.2020, w tym Urlop Macierzyński: 08.2019-08.2020)",
+        jobDescription:
+            [
+                "Umiejętność pracy w dynamicznie zmieniającym się środowisku pracy oraz elastyczne podejście do priorytetów i planów",
+                "Umiejętność pracy oraz podejmowania decyzji pod presją",
+                "Rekrutacja, szkolenia, ocena i rozwój doradców sprzedaży w zespole",
+                "Zarządzanie zespołem wraz z planowaniem czasu pracy sobie i innym członkom zespołu",
+                "Nadzorowanie pracy oraz przekazywanie feedbacków",
+                "Inicjowanie oraz kontrolowanie działań mających na celu zapewnienie zysku i zapobieganie stratom",
+                "Umiejętność planowania i reagowania na dynamicznie rozwijające się sytuacje poprzez ustalanie odpowiednich priorytetów",
+                "Analiza i reakcja na raporty sprzedaży",
+                "Planowanie i zarządzanie administracją personalną zespołu m.in. grafikami, urlopami czy badaniami lekarskimi"
+            ]
+    },
+    {
+        jobTitle: "Lider Zmiany w H&M",
+        place: "Szczecin (02.2018 - 05.2018)",
+        jobDescription:
+            [
+                "Wdrażanie nowych pracowników",
+                "Zarządzanie zespołem",
+                "Praca z wydajnością i jakością obsługi klienta",
+                "Wykonywanie zadań związanych z operacjami gotówkowymi oraz księgowością",
+                "Wspomaganie pozostałych członków zespołu w czynnościach administracyjno-kasowych oraz z zakresu BHP"
+            ]
+    },
+    {
+        jobTitle: "Doradca Sprzedaży w H&M",
+        place: "Centrum Handlowe Galaxy, Szczecin (07.2015 - 05.2018)",
+        jobDescription:
+            [
+                "Obsługa klienta",
+                "Dbanie o odpowiednie zatowarowanie sklepu",
+                "Przyjmowanie i rozpakowywanie dostawy",
+                "Prezentacja towaru w sklepie zgodnie ze standardami firmy"
+            ]
+    },
+    {
+        jobTitle: "Kasjer-Sprzedawca w Reserved",
+        place: "Centrum Handlowe Kaskada, Szczecin (03.2014 - 06.2015)",
+        jobDescription:
+            ["Obsługa klienta",
+                "Obsługa kasy",
+                "Dbanie o wizerunek firmy",
+                "Praca zespołowa",
+                "Praca z towarem na sklepie i na magazynie",
+            ]
+    },
+    {
+        jobTitle: "Barmanka",
+        place: "Restauracja Bombay, Szczecin (10.2013 - 02.2014)",
+        jobDescription:
+            [
+                "Przygotowanie oraz organizacja stanowiska pracy",
+                "Praca zespołowa i efektywna komunikacja z resztą zespołu",
+                "Kontrola stanów magazynowych",
+                "Praca zgodnie ze standardami firmy",
+            ]
+    }
+]
