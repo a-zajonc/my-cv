1fa679ae6960df7bd4a3a7632f476fcaaf79bd39
Ada <byjean94@gmail.com>
2022-08-22 11:08:56 +0200

    Remove Repetition in Education Section

--- src/Components/SecondSection/Education/index.js
+++ src/Components/SecondSection/Education/index.js
@@ -1,22 +1,25 @@
 import { Box, Heading, UnorderedList, ListItem, Text } from '@chakra-ui/react';
 import { Header } from '../Styling';
+import { studies } from '../../../database';
 
 export function Education() {
 
-    function studiesDisplay(department, studies, years) {
+    function studiesDisplay(department, studiesName, years) {
+
         return <ListItem>
             <Text fontSize="14px" fontFamily="Montserrat" color="#494E5F" fontWeight="300" >
                 {department}
             </Text>
             <Box display="flex" justifyContent="space-between">
                 <Heading fontSize="22px" fontFamily="Montserrat" color="#494E5F" pt="3">
-                    {studies}
+                    {studiesName}
                 </Heading>
                 <Text fontFamily="Montserrat" color="#494E5F" fontSize="22px" pt="3">
                     {years}
                 </Text>
             </Box>
         </ListItem>
+
     }
 
     return <Box p="5">
@@ -25,8 +28,11 @@ export function Education() {
             Uniwersytet Szczeciński
         </Heading>
         <UnorderedList pl="3">
-            {studiesDisplay(("Wydział Nauk Ekonomicznych i Zarządzania"), ("Magister Public Management"), ("2016-2018"))}
-            {studiesDisplay(("Wydział Filologiczny"), ("Licencjat Filologii angielskiej"), ("2013-2016"))}
+            {studies.map((study) => {
+                return studiesDisplay((study.department), (study.studiesName), (study.years))
+            }
+            )
+            }
         </UnorderedList>
     </Box>
 }
--- src/database.js
+++ src/database.js
@@ -62,4 +62,23 @@ export const work = [
     }
 ]
 
-export const hobbies = ["czytanie książek po polsku i po angielsku, głównie z gatunku literatury faktu, popularnonaukowych, podróżniczych, kryminały i poradniki", "piłka nożna, w szczególności Manchester United", "filmy i seriale", "gry komputerowe, głównie takie z otwartym światem"]
+export const hobbies =
+    [
+        "czytanie książek po polsku i po angielsku, głównie z gatunku literatury faktu, popularnonaukowych, podróżniczych, kryminały i poradniki",
+        "piłka nożna, w szczególności Manchester United",
+        "filmy i seriale",
+        "gry komputerowe, głównie takie z otwartym światem"
+    ]
+
+export const studies = [
+    {
+        department: "Wydział Nauk Ekonomicznych i Zarządzania",
+        studiesName: "Magister Public Management",
+        years: "2016-2018"
+    },
+    {
+        department: "Wydział Filologiczny",
+        studiesName: "Licencjat Filologii angielskiej",
+        years: "2013-2016"
+    }
+]
