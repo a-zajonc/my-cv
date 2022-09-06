573f8646bf41730151f645429154b69bfa8f69da
Ada <byjean94@gmail.com>
2022-09-06 11:11:33 +0200

    Add i18next library

--- src/Components/SecondSection/Hobbies/index.js
+++ src/Components/SecondSection/Hobbies/index.js
@@ -2,6 +2,7 @@ import { Box, UnorderedList, ListItem } from '@chakra-ui/react';
 import { Header } from '../Styling';
 
 export function Hobbies({ hobbies }) {
+
     return <Box p="5">
         <Header title={"Zainteresowania"} />
         <UnorderedList fontFamily="Montserrat" color="#494E5F" pt="3" pl="3" fontSize="18px">
--- src/Components/SecondSection/index.js
+++ src/Components/SecondSection/index.js
@@ -2,13 +2,17 @@ import { Box } from '@chakra-ui/react';
 import { Education } from './Education';
 import { Employment } from './Employment';
 import { Hobbies } from './Hobbies';
-import { hobbies } from '../../database';
+import { useTranslation } from 'react-i18next';
 
 export function SecondSection() {
+
+    const { t } = useTranslation();
 
     return <Box width="60%">
         <Employment />
         <Education />
-        <Hobbies hobbies={hobbies} />
+        <Hobbies hobbies={t('hobbies', { returnObjects: true })} />
     </Box>
 }
+
+// i18next.t('hobbies', { returnObjects: true })
